7065f30d453b7fcaf84a83cbd4dfe2a2c6749cd1
“Abdul <kaazees31@gmail.com>
2024-03-04 18:20:35 +0530

    notes

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,14 +1,10 @@
-// Importing express module using ES6 module syntax
 import express from "express";
 
-// Importing the notesController module from the specified path
 import notes from "../controllers/notesController.js";
 
-// Creating a new router instance
 const Router = express.Router();
 
-// Defining a POST route for '/notes' and associating it with the 'notes' controller
 Router.post("/notes", notes);
+Router.get("/notes", notes);
 
-// Exporting the router instance
 export default Router;
